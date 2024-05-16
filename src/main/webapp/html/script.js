cfffa5cba26be8b80389d41c45e7ac1b3a4d3548
oleksandrhavryush <havryush@gmail.com>
2024-05-16 11:08:23 +0200

    Updated functionality to fetch data from "/rest/players" endpoint
    Updated a function to send a GET request to the "/rest/players" URL and append the resulting data to the table specified in point 7 of the requirements. Upon restarting the server, the table now displays four rows: the first row contains column names, and the subsequent rows contain data fetched from the server.

--- src/main/webapp/html/script.js
+++ src/main/webapp/html/script.js
@@ -1,3 +1,32 @@
+fillTable()
+
+function fillTable() {
+    $.get(`rest/players`, (players) => {
+        console.log(players);
+
+        const $playersTableBody = $('.players-table-body')[0];
+        let htmlRows = '';
+
+        players.forEach((player) => {
+            htmlRows +=
+                `<tr>
+                    <td class="cell">${player.id}</td>
+                    <td class="cell">${player.name}</td>
+                    <td class="cell">${player.title}</td>
+                    <td class="cell">${player.race}</td>
+                    <td class="cell">${player.profession}</td>
+                    <td class="cell">${player.level}</td>
+                    <td class="cell">${player.birthday}</td>
+                    <td class="cell">${player.banned}</td>
+                </tr>`
+        })
+
+        $playersTableBody.insertAdjacentHTML('beforeend', htmlRows);
+    })
+}
+
+
+/*
 let accountsCount = 0;
 let currentPageNumber = 0;
 let accountsPerPage = 3;
@@ -78,9 +107,17 @@ function createTableRow(account) {
                 <td>${account.level}</td>
                 <td>${account.birthday}</td>
                 <td>${account.banned}</td>
+                <td><button onclick="editAccount(${account.id})">Edit</button></td>
+                <td><button onclick="deleteAccount(${account.id})">Delete</button></td>
             </tr>`;
 }
 
 function deleteAccount(id) {
-    $.delete(`http://localhost:8080/rest/players/${id}`, () => updateAccountsTable())
+    // Видаляємо обліковий запис
+    $.ajax({
+        url: `http://localhost:8080/rest/players/${id}`,
+        type: 'DELETE',
+        success: () => updateAccountsTable()
+    });
 }
+*/
